a078894c3f1da40e5e4bebca8b8318ec71cf4d7a
Spas <spas.shopov@dnhsoft.com>
2017-06-05 16:45:48 +0300

    read shopId from config

--- src/Controllers/ProtectedShopsController.php
+++ src/Controllers/ProtectedShopsController.php
@@ -2,14 +2,15 @@
 
 namespace ProtectedShops\Controllers;
 
-
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
+use Plenty\Plugin\ConfigRepository;
 
 class ProtectedShopsController extends Controller
 {
-    public function protectedShopsInfo(Twig $twig):string
+    public function protectedShopsInfo(Twig $twig, ConfigRepository $config):string
     {
-        return $twig->render('ProtectedShopsForPlenty::content.info');
+        $data['shopId'] = $config->get('ProtectedShopsForPlenty.shopId');
+        return $twig->render('ProtectedShopsForPlenty::content.info', $data);
     }
 }
