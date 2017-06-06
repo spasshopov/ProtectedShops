b408f3683f78bd3251fdac4b00475c3a68ef1ca0
Spas <spas.shopov@dnhsoft.com>
2017-06-06 14:08:50 +0300

    removed initializing service with new, moved methodfs in controller

--- src/Controllers/ProtectedShopsController.php
+++ src/Controllers/ProtectedShopsController.php
@@ -5,23 +5,58 @@ namespace ProtectedShops\Controllers;
 use Plenty\Plugin\Controller;
 use Plenty\Plugin\Templates\Twig;
 use Plenty\Plugin\ConfigRepository;
-use ProtectedShops\Service\ProtectedShopsDocumentService;
 
 class ProtectedShopsController extends Controller
 {
     /**
-     * @var ProtectedShopsDocumentService
+     * @var string
      */
-    private $psService;
+    private $apiUrl;
 
     public function protectedShopsInfo(Twig $twig, ConfigRepository $config):string
     {
-        $this->psService = new ProtectedShopsDocumentService();
-
         $shopId = $config->get('ProtectedShopsForPlenty.shopId');
         $data['shopId'] = $shopId;
-        $data['document'] = $this->psService->getDocument($shopId, 'agb');
+        $data['document'] = $this->getDocument($shopId, 'agb');
 
         return $twig->render('ProtectedShopsForPlenty::content.info', $data);
+    }
+
+    /**
+     * @param $shopId
+     * @param $documentType
+     * @return string
+     */
+    public function getDocument($shopId, $documentType)
+    {
+        $apiFunction = 'documents/' . $documentType . '/contentformat/html';
+        $response = $this->apiRequest($shopId, $apiFunction);
+
+        return $response;
+    }
+
+    /**
+     * @param $shopId
+     * @param $apiFunction
+     * @return mixed
+     */
+    private function apiRequest($shopId, $apiFunction)
+    {
+        $dsUrl = "https://$this->apiUrl/v2.0/de/partners/demo/shops/$shopId/$apiFunction/format/json";
+
+        // Open connection
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $dsUrl);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $response = curl_exec($ch);
+
+        //close connection
+        curl_close($ch);
+
+        return $response;
     }
 }
