0b04c7a08c64cce4af8bb343e1b46aa1d6d0c76e
Spas <spas.shopov@dnhsoft.com>
2017-06-16 13:37:40 +0300

    Plugin ready

--- src/Providers/ProtectedShopsServiceProvider.php
+++ src/Providers/ProtectedShopsServiceProvider.php
@@ -8,15 +8,6 @@ use ProtectedShops\Cron\ProtectedShopsCronHandler;
 
 class ProtectedShopsServiceProvider extends ServiceProvider
 {
-    /**
-     * @var string
-     */
-    private $apiUrl = 'api.protectedshops.de';
-
-
-    /**
-     *
-     */
     public function register()
     {
         $this->getApplication()->register(ProtectedShopsRouteServiceProvider::class);
@@ -27,45 +18,6 @@ class ProtectedShopsServiceProvider extends ServiceProvider
      */
     public function boot(CronContainer $container)
     {
-        $container->add(CronContainer::EVERY_FIFTEEN_MINUTES, ProtectedShopsCronHandler::class);
-        $this->getDocument('61BBBE9D2FB4959AA74D03FDF2F6484D', 'AGB');
-    }
-
-    /**
-     * @param $shopId
-     * @param $documentType
-     * @return string
-     */
-    private function getDocument($shopId, $documentType):string
-    {
-        $apiFunction = 'documents/' . $documentType . '/contentformat/html';
-        $response = $this->apiRequest($shopId, $apiFunction);
-
-        return $response;
-    }
-
-    /**
-     * @param $shopId
-     * @param $apiFunction
-     * @return mixed
-     */
-    private function apiRequest($shopId, $apiFunction):string
-    {
-        $dsUrl = "https://$this->apiUrl/v2.0/de/partners/protectedshops/shops/$shopId/$apiFunction/format/json";
-
-        // Open connection
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, $dsUrl);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        $response = curl_exec($ch);
-
-        //close connection
-        curl_close($ch);
-
-        return $response;
+        $container->add(CronContainer::DAILY, ProtectedShopsCronHandler::class);
     }
 }
